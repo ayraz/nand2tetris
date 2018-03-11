8bf8be944726320a85a559609a7cd0857bc4ae9d
Ayraz <artem.ayra@gmail.com>
2018-03-11 18:57:01 +0100

    Weeks 4 and 5 of coursework.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,59 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// R0 will hold color bits
+(SETW)
+    @R0 
+    M=0
+    @DRAW
+    0;JMP
+    
+(SETB)
+    @R0 
+    M=-1
+    @DRAW
+    0;JMP
+
+(LOOP)
+    // init i for drawing loop
+    @i
+    M=0
+
+    // if no key is pressed, set bits to white and draw
+    @KBD
+    D=M
+    @SETW
+    D;JEQ
+    // else to black
+    @SETB
+    0;JMP
+    
+    (DRAW)
+        // current pixel address
+        @i
+        D=M
+        @SCREEN
+        D=D+A
+        
+        // store for later
+        @R1
+        M=D
+        
+        @KBD
+        D=A-D // last address of screen
+        @LOOP
+        D;JLE
+        
+        // write color bits
+        @R0
+        D=M
+        @R1
+        A=M
+        M=D
+        
+        @i
+        M=M+1
+        
+        @DRAW
+        0;JMP
+    
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,43 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// i = 0
+// a = R0
+// b = R1
+// product = 0;
+// while (i < b) {
+//     product += a;
+// }
+// R2 = A
+
+@i
+M=0
+
+@sum
+M=0
+
+(LOOP)
+    @i
+    D=M
+    @R0
+    D=M-D
+    @END
+    D;JLE
+
+    @R1
+    D=M
+    @sum
+    M=D+M
+    
+    @i
+    M=M+1
+    
+    @LOOP
+    0;JMP
+(END)
+    @sum
+    D=M
+    @R2
+    M=D
+    @END
+    0;JMP
